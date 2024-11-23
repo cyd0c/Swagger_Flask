e163d81b3ed53562d9b4f48adb24954c40c404e7
alnsaji <108118855+cyd0c@users.noreply.github.com>
2024-11-23 11:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
 import connexion
-
 
 # Create the Connexion app
 app = connexion.FlaskApp(__name__, specification_dir='./')
+
+# Enable CORS for specific origins
+CORS(app.app, resources={r"/api/*": {"origins": ["http://localhost:8000", "http://127.0.0.1:8000"]}})
 
 # Add the OpenAPI specification (your YAML file)
 app.add_api('swagger.yaml')
@@ -11,4 +15,4 @@ app.add_api('swagger.yaml')
 app.app.config['DEBUG'] = True
 
 # Run the app
-app.run()
+app.run(port=8000)
